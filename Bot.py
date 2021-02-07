30cc19c46e8d1f6449420c19a677dff4eadeeacf
CoockieVii <valitovilmir29@gmail.com>
2021-02-07 13:17:57 +0300

    Связал бота с телеграмм

--- Bot.py
+++ Bot.py
@@ -5,11 +5,10 @@ import Expenditure
 token = "1627587497:AAHhsD5XTu4m_A5ZQUdC6yXisUdyOfXrzfk"
 bot = telebot.TeleBot(token)
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-	bot.reply_to(message, "Кулити")
+    bot.reply_to(message, "Кулити")
 
 
-
-
-
+bot.polling()

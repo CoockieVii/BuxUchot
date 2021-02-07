7e114813d02e2af92b9c8e4c38a3fc7489aa05a6
CoockieVii <valitovilmir29@gmail.com>
2021-02-07 12:58:18 +0300

    add classes User,Uchot,Expenditure

---
+++ Uchot.py
@@ -0,0 +1,14 @@
+from User import User
+
+
+class Uchot:
+
+    def __init__(self, user1: User, user2: User):
+        self.user1 = user1
+        self.user2 = user2
+        self.expenditures = []
+
+    def __add_expenditure(Expenditure):
+        Expenditure.append(Expenditure)
+
+    pass
